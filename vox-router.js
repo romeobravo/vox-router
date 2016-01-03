e913e82265efaf15bae51876803543232468403c
Ruben Buitelaar <rubenbuitelaar@gmail.com>
2016-01-03 21:50:45 +0100

    browserify to babel

---
+++ vox-router.js
@@ -0,0 +1,69 @@
+"use strict";
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Node = (function () {
+  function Node(settings) {
+    _classCallCheck(this, Node);
+
+    this.component = settings.original.component;
+    this.type = settings.original.type;
+    this.name = settings.edge;
+    this.parent = settings.parent;
+    this.children = [];
+  }
+
+  _createClass(Node, [{
+    key: "addChild",
+    value: function addChild(childNode) {
+      this.children.push(childNode);
+    }
+  }]);
+
+  return Node;
+})();
+
+exports.default = Node;
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _Node = require('./Node');
+
+var _Node2 = _interopRequireDefault(_Node);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function parseRoutes(edge, original, parent) {
+  var node = new _Node2.default({
+    original: original,
+    parent: parent,
+    name: edge
+  });
+
+  if (original.children) {
+    for (var i in original.children) {
+      var child = parseRoutes(i, original.children[i], node);
+      node.addChild(child);
+    }
+  }
+  return node;
+}
+
+var VoxRouter = function VoxRouter(settings) {
+  _classCallCheck(this, VoxRouter);
+
+  this.root = parseRoutes('', settings.routes, null);
+};
+
+exports.default = VoxRouter;
