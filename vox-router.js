086a16ffae03fdc78e8650d948404d83a250211e
Ruben Buitelaar <rubenbuitelaar@gmail.com>
2016-01-03 14:59:07 +0100

    init

---
+++ vox-router.js
@@ -0,0 +1,25 @@
+import Node from './lib/Node'
+
+function parseRoutes(edge, original, parent) {
+  let node = new Node({
+    original: original,
+    parent: parent,
+    name: edge
+  })
+
+  if(original.children) {
+    for(let i in original.children) {
+      let child = parseRoutes(i, original.children[i], node)
+      node.addChild(child)
+    }
+  }
+  return node
+}
+
+class VoxRouter {
+  constructor(settings) {
+    this.root = parseRoutes('', settings.routes, null)
+  }
+}
+
+export default VoxRouter
